fcda0be70c3325e1a29d422e821b1aff87591b54
Lemaire Wouter <wouterlemaire120@hotmail.com>
2019-01-28 22:32:47 +0000

    added session fixed participants counter

--- webapp/controllers/Main.controller.js
+++ webapp/controllers/Main.controller.js
@@ -1,19 +1,21 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller"],
-    function(Controller){
-        let Main = Controller.extend("be.fiddle.sitbe.controllers.Main",
-        /** @lends be.fiddle.sitbe.controller.Main.prototype */
-        {
-        	
-        });
+	["sap/ui/core/mvc/Controller"],
+	function (Controller) {
+		let Main = Controller.extend("be.fiddle.sitbe.controllers.Main",
+			/** @lends be.fiddle.sitbe.controller.Main.prototype */
+			{
 
-        Main.prototype.getLength = function(array){
-            if(Array.isArray(array)){
-                return "" + array.length;
-            }
-            return "0";
-        }
+			});
 
-        return Main;
-    }
+		Main.prototype.onInit = function () {
+			this.getView().bindElement({
+				path: 'participants>/Events(59)',
+				parameters:{
+					expand:'Participants'
+				}});
+		};
+
+
+		return Main;
+	}
 );
