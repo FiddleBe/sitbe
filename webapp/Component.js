a58cbd9ef2214e02be2e6668bdb6aa713f9f5c9b
Tom Van Doorslaer <Tom@fiddle.be>
2019-05-06 12:03:39 +0200

    converted session list to an agenda.

--- webapp/Component.js
+++ webapp/Component.js
@@ -15,11 +15,14 @@ sap.ui.define([
 
 	Component.prototype.init = function() {
 		UIComponent.prototype.init.apply(this, arguments);
+
+		this.getModel("settings").setProperty("/startdate", new Date("2019-05-18T09:00:00"));
+
 		// Remove the splash screen
-		    $(".loader-icon").removeClass("spinning-cog").addClass("shrinking-cog");
-		    $(".loader-background").fadeOut();
-		    $(".loader-text").fadeOut();
-		    window.setTimeout(function() { $("#loader").remove(); }, 400);
+		$(".loader-icon").removeClass("spinning-cog").addClass("shrinking-cog");
+		$(".loader-background").fadeOut();
+		$(".loader-text").fadeOut();
+		window.setTimeout(function() { $("#loader").remove(); }, 400);
 	};
 
     return Component;
--- webapp/controllers/Main.controller.js
+++ webapp/controllers/Main.controller.js
@@ -12,9 +12,25 @@ sap.ui.define(
 				path: 'participants>/Events(59)',
 				parameters:{
 					expand:'Participants'
-				}});
+				}
+			});
+			
 		};
 
+		Main.prototype.onAfterRendering = function(event){
+			this.getView().getModel("info").attachRequestCompleted({}, this.updateSessionDates, this );
+		};
+
+		Main.prototype.updateSessionDates = function(event){
+			//convert the json dates, because the calendar element is being a bitch
+			let sessions = this.getView().getModel("info").getProperty("/sessions");
+			sessions && sessions.forEach(function(session){
+				session.startDate = new Date(session.startdate);
+				session.endDate = new Date(session.enddate);
+			});
+
+			this.getView().getModel("info").setProperty("/sessions",sessions);
+		};
 
 		return Main;
 	}
